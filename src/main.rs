c30ab624cbbb444d3eaf15156c9bb2cbed5d8108
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2024-06-14 02:14:30 +0200

    (Add example code to main)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use mpa_lib::mp_uint::MPuint;
+
 fn main() {
-    println!("Hello, world!");
+    let two = MPuint::two(128);
+    let div_two_by_three = &two / 3;
+    let mod_two_by_three = &two % 3;
+    println!("→→→→ two / 3 = {:?}", div_two_by_three); // >>> [0,0]
+    println!("→→→→ two % 3 = {:?}", mod_two_by_three); // >>> 2
+
+    let ten = MPuint::ten(128);
+    let div_ten_by_three = &ten / 3;
+    let mod_ten_by_three = &ten % 3;
+    println!("→→→→ ten / 3 = {:?}", div_ten_by_three); // >>> [3,0]
+    println!("→→→→ ten % 3 = {:?}", mod_ten_by_three); // >>> 1
 }
