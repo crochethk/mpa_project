634a57b614e1f838794cd9df97697448b442c02e
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2024-06-27 23:16:10 +0200

    (Update main.rs code example)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use mpa_lib::mp_int::MPint;
 
 fn main() {
-    let two = MPint::two(128);
+    let two = MPint::from_digit(2, 128);
     let div_two_by_three = &two / 3;
     let mod_two_by_three = &two % 3;
     println!("→→→→ two / 3 = {:?}", div_two_by_three); // >>> [0,0]
     println!("→→→→ two % 3 = {:?}", mod_two_by_three); // >>> 2
 
-    let ten = MPint::ten(128);
+    let ten = MPint::from_digit(10, 128);
     let div_ten_by_three = &ten / 3;
     let mod_ten_by_three = &ten % 3;
     println!("→→→→ ten / 3 = {:?}", div_ten_by_three); // >>> [3,0]
@@ -17,6 +17,12 @@ fn main() {
     let num_str = "1234";
     let width = 123;
     let num = MPint::from_str(num_str, width).unwrap();
-    println!("{:?}", num); // >>> MPuint { width: 128, data: [1234, 0] }
-    println!("{}", num); // >>> 0000 ... 0000 0100 1101 0010
+    println!("{:?}", num); // >>> MPint { width: 128, data: [1234, 0], sign: Pos }
+    println!("{}", num); // >>> 0000000000000000 00000000000004D2
+
+    let num_str = "-1234";
+    let width = 123;
+    let num = MPint::from_str(num_str, width).unwrap();
+    println!("{:?}", num); // >>> MPint { width: 128, data: [1234, 0], sign: Neg }
+    println!("{}", num); // >>> -0000000000000000 00000000000004D2
 }
