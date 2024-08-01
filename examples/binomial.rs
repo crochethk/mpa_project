fd051990b5817bcbb2cd62de25feedee7ebf415d
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2024-08-01 02:21:23 +0200

    Refactor unittests of `binomial` example

--- examples/binomial.rs
+++ examples/binomial.rs
@@ -1,3 +1,7 @@
+//! This example implements one of the core ingredients in combinatorics: the
+//! __binomial coefficient__. It is done utilizing _Pascal's Triangle_, which
+//! basically is the _inductive definition_ of binomial coefficients.
+
 use mpa_lib::mp_int::*;
 
 /// Caculates the binomial coefficient `(n|k)` ("n choose k") using *Pascal's triangle*.
@@ -22,8 +26,6 @@ fn binomial(n: u64, mut k: u64) -> MPint {
 }
 
 fn main() {
-    test_mpint_binom();
-
     let n = 10000;
     let k = 42;
     let result = binomial(n, k);
@@ -33,34 +35,40 @@ fn main() {
     println!("(n|k) = ({n} | {k}) = {result}")
 }
 
-fn test_mpint_binom() {
-    // test triangle middle value
-    assert_eq!(binomial(4, 2), 6);
-    assert_eq!(binomial(2, 1), 2);
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    // test triangle symmetry
-    assert_eq!(binomial(4, 1), 4);
-    assert_eq!(binomial(4, 3), 4);
-    assert_eq!(binomial(5, 2), 10);
-    assert_eq!(binomial(5, 3), 10);
-    assert_eq!(binomial(5, 1), binomial(5, 4));
+    #[test]
+    pub fn test_mpint_binom() {
+        // test triangle middle value
+        assert_eq!(binomial(4, 2), 6);
+        assert_eq!(binomial(2, 1), 2);
 
-    // test triangle boundaries
-    assert_eq!(binomial(6, 0), 1);
-    assert_eq!(binomial(6, 6), 1);
+        // test triangle symmetry
+        assert_eq!(binomial(4, 1), 4);
+        assert_eq!(binomial(4, 3), 4);
+        assert_eq!(binomial(5, 2), 10);
+        assert_eq!(binomial(5, 3), 10);
+        assert_eq!(binomial(5, 1), binomial(5, 4));
 
-    // test big values
-    let b = binomial(100, 49);
-    let expected = MPint::from_dec_str("98913082887808032681188722800").unwrap();
-    assert_eq!(b, expected);
+        // test pascal's triangle boundaries
+        assert_eq!(binomial(6, 0), 1);
+        assert_eq!(binomial(6, 6), 1);
 
-    let b = binomial(1000, 42);
-    let dec_str = "297242911333923795640059429176065863139989673213703918037987737481286092000";
-    let expected = MPint::from_dec_str(dec_str).unwrap();
-    assert_eq!(b, expected);
+        // test big values
+        let b = binomial(100, 49);
+        let expected = MPint::from_dec_str("98913082887808032681188722800").unwrap();
+        assert_eq!(b, expected);
 
-    let b = binomial(10000, 42);
-    let dec_str = "652945894617920230937165266791194236925532551338164487629981313209097028731092090795095199544337067516501827475720000";
-    let expected = MPint::from_dec_str(dec_str).unwrap();
-    assert_eq!(b, expected);
+        let b = binomial(1000, 42);
+        let dec_str = "297242911333923795640059429176065863139989673213703918037987737481286092000";
+        let expected = MPint::from_dec_str(dec_str).unwrap();
+        assert_eq!(b, expected);
+
+        let b = binomial(10000, 42);
+        let dec_str = "652945894617920230937165266791194236925532551338164487629981313209097028731092090795095199544337067516501827475720000";
+        let expected = MPint::from_dec_str(dec_str).unwrap();
+        assert_eq!(b, expected);
+    }
 }
